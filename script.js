df4b70b98d2f44f0ecf7e1cea94b61bee6559f75
TMcClure123 <jumpscares4days@gmail.com>
2023-03-01 10:44:08 -0500

    Commit 2

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-        if(userInput==='rock' ||
-         userInput==='paper' ||
-         userInput ==='scissors'){
-       return userInput;
-       }else  {
-           console.log('Invalid input');
-        return 'Check your input'
-       } 
-  };
-  
-  function getComputerChoice(){
-    const choiceVariable= Math.floor(Math.random()*3);
-    switch(choiceVariable){
-        case 0:
-        return 'rock';
-        break;
-        case 1:
-        return 'paper';
-        break;
-        case 2:
-        return 'scissors';
-        break;
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("Invalid input");
+    return "Check your input";
+  }
+};
+
+function getComputerChoice() {
+  const choiceVariable = Math.floor(Math.random() * 3);
+  switch (choiceVariable) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+      break;
+  }
+}
+let userScore = 0;
+let computerScore = 0;
+
+const determineWinner = (userChoice, computerChoice) => {
+if (userChoice === computerChoice) {
+  return document.getElementById("result").innerHTML = "You tied with the computer!";
+}
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      computerScore++;
+      document.getElementById('computerSc ore').innerHTML = computerScore;
+      return document.getElementById("result").innerHTML = "The computer won!";
+      //change HTML
+    } else if (computerChoice === "scissors") {
+      userScore++;
+      document.getElementById('userScore').innerHTML = userScore;
+      return document.getElementById("result").innerHTML = "You won!";
+    } 
+  } else if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+      userScore++;
+      document.getElementById('userScore').innerHTML = userScore;
+      return document.getElementById("result").innerHTML = "You won!";
+    } else if (computerChoice === "scissors") {
+      computerScore++;
+      document.getElementById('computerScore').innerHTML = computerScore;
+      return document.getElementById("result").innerHTML = "The computer won!";
+    }
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      computerScore++;
+      document.getElementById('computerScore').innerHTML = computerScore;
+      return document.getElementById("result").innerHTML = "The computer won!";
+    } else if (computerChoice === "paper") {
+      userScore++;
+      document.getElementById('userScore').innerHTML = userScore;
+      return document.getElementById("result").innerHTML = "You won!";
     }
   }
   
-    if(userChoice === 'rock'){
-      if(computerChoice === 'paper'){
-         return 'computer won';
-         //change HTML
-      }else if (computerChoice ==='scissors'){
-         return 'user won';
-      }
-    }else if(userChoice === 'paper') {
-      if(computerChoice === 'rock'){
-         return 'user won';
-         }else if (computerChoice === 'scissors'){
-           return 'computer won';
-         }
-    }else if(userChoice === 'scissors'){
-      if(computerChoice === 'rock'){
-        return 'computer won';
-      }else if (computerChoice === 'paper'){
-        return 'user won';
-      }   
-    }
-  function playGame(input){
-    const userChoice = getUserChoice(input);
-    const computerChoice = getComputerChoice();
-    console.log('User choice is: ' + userChoice);
-    console.log('Computer choice is: ' + computerChoice);
-  console.log(determineWinner(userChoice,computerChoice));
-  }
-  
+}
+
+function playGame(choice) {
+  const userChoice = getUserChoice(choice);
+  const computerChoice = getComputerChoice();
+  console.log("User choice is: " + userChoice);
+  console.log("Computer choice is: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
